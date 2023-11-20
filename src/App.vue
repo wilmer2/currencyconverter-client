5858d53257b8fec7cd0a2e08357ff22697e84b57
Wilmer Maldonado <wilmer.payall@gmail.com>
2023-11-20 19:21:46 -0400

    create login and register form

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
 import Header from './components/Header.vue';
 import Container from './components/Container.vue';
 </script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="w-full max-w-md mx-auto rounded-lg">
+		<form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+			<div class="mb-4">
+				<label
+					class="block text-gray-700 text-sm font-bold mb-2"
+					for="username"
+				>
+					Correo
+				</label>
+				<input
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+					id="username"
+					type="text"
+					placeholder="Correo"
+				/>
+			</div>
+			<div class="mb-6">
+				<label
+					class="block text-gray-700 text-sm font-bold mb-2"
+					for="password"
+				>
+					Contraseña
+				</label>
+				<input
+					class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+					id="password"
+					type="password"
+					placeholder="******************"
+				/>
+				<p class="text-red-500 text-xs italic">Please choose a password.</p>
+			</div>
+			<div class="flex items-center justify-center">
+				<button
+					class="rounded-md bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+					type="button"
+				>
+					Iniciar sesión
+				</button>
+			</div>
+		</form>
+	</div>
+</template>
